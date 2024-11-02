666f87073e3b6077065238079a80c78da2350c6d
saeed alzahrani <saeedappapp@users.noreplay.github.com>
2024-11-02 19:33:46 +0300

    fondation scene

--- vn-cpit252/src/main/java/org/example/vncpit252/HelloApplication.java
+++ vn-cpit252/src/main/java/org/example/vncpit252/HelloApplication.java
@@ -2,6 +2,7 @@ package org.example.vncpit252;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root =  FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("CPIT252 VN");
         stage.setScene(scene);
         stage.show();
     }
--- vn-cpit252/src/main/java/org/example/vncpit252/HelloController.java
+++ vn-cpit252/src/main/java/org/example/vncpit252/HelloController.java
@@ -1,14 +1,31 @@
 package org.example.vncpit252;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class HelloController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    public void switchToScene1(ActionEvent event) throws IOException {
+        FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+    }
+    public void switchToScene2(ActionEvent event){
+
     }
 }
