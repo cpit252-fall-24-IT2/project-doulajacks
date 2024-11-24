17956f4d7125e76926ca66742cb7cb3042223e4e
joseph677 <joseph677@users.noreply.github.com>
2024-11-24 21:12:35 +0300

    Added some button functionality and saving user data

--- vn-cpit252/src/main/java/org/example/vncpit252/Controller.java
+++ vn-cpit252/src/main/java/org/example/vncpit252/Controller.java
@@ -1,5 +1,6 @@
 package org.example.vncpit252;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,34 +9,72 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 
 public class Controller {
+
     private Stage stage;
     private Scene scene;
     private Parent root;
+
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private TextField nameEntryID;
+
+    @FXML
+    private TextField emailEntryID;
+
+    @FXML
+    private ChoiceBox<?> genderChoiceID;
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    void saveInfo(ActionEvent event) throws IOException {
+        SaveInfo currentInfo = new SaveInfo.SaveInfoBuilder(nameEntryID.getText(), emailEntryID.getText(), "male" /* Change this to ChoiceBox value */).build();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setUserData(currentInfo);
+        switchScene(event, "save home page.fxml");
+    }
+
+    @FXML
+    void printInfo(ActionEvent event) {
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        System.out.println(stage.getUserData());
+    }
+
     public void switchToSceneHomeSave(@SuppressWarnings("exports") ActionEvent event) throws IOException {
-       root= FXMLLoader.load(getClass().getResource("save home page.fxml"));
-       stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-       scene = new Scene(root);
-       stage.setScene(scene);
-       stage.show();
+       switchScene(event, "save home page.fxml");
     }
     @SuppressWarnings("exports")
     public void switchToHomePage(ActionEvent event) throws IOException{
-        root= FXMLLoader.load(getClass().getResource("home page.fxml"));
+        switchScene(event, "home page.fxml");
+    }
+
+    @FXML
+    void switchToInfoPage(ActionEvent event) throws IOException {
+        switchScene(event, "info-builder-colliction.fxml");
+    }
+
+    public void switchScene(ActionEvent event, String fxmlname) throws IOException {
+        root= FXMLLoader.load(getClass().getResource(fxmlname));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void exit() {
+        Platform.exit();
     }
 }
