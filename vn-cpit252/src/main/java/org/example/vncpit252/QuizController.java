817466b5a0e89b18b25b51b3817df099e88bc02e
Faris 21 <you@example.com>
2024-12-01 19:26:26 +0300

    commit

--- vn-cpit252/src/main/java/org/example/vncpit252/QuizController.java
+++ vn-cpit252/src/main/java/org/example/vncpit252/QuizController.java
@@ -1,10 +1,14 @@
 package org.example.vncpit252;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class QuizController {
     String[] questions = SharedData.getQuestionStrings();
@@ -30,6 +34,8 @@ public class QuizController {
     @FXML
     private Label feedbackLabel;
 
+    @FXML
+    private Button backButton;
     
     
 
@@ -72,5 +78,11 @@ public class QuizController {
             feedbackLabel.setText("Quiz Completed!");
             nextButton.setDisable(true); // Disable the "Next" button after the last question
         }
+    }
+    @FXML
+    private void handleBackButton(ActionEvent event) throws IOException {
+        Controller controller = new Controller();
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        controller.switchScene(event, "load_home_page.fxml");
     }
 }
