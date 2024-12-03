ac090f0f454431f976436a539949240329a9ef78
joseph677 <joseph677@users.noreply.github.com>
2024-12-03 22:37:49 +0300

    Everything works now on learning materials

--- vn-cpit252/src/main/java/org/example/vncpit252/ResourcesController.java
+++ vn-cpit252/src/main/java/org/example/vncpit252/ResourcesController.java
@@ -1,28 +1,22 @@
 package org.example.vncpit252;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.web.WebView;
 import javafx.scene.input.MouseEvent;
 import javafx.event.ActionEvent;
-import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class ResourcesController {
 
     @FXML
-    private AnchorPane paneID;
-
-    @FXML
     private WebView webHolderID;
-
-    @FXML
-    private Label label1ID;
 
     @FXML
     private VBox videosVboxID;
@@ -51,24 +45,54 @@ public class ResourcesController {
 
         for (String link: resources.getReadings()) {
             Hyperlink hl = new Hyperlink(link);
+            // open link in default browser
+            hl.setOnAction(mouseEvent -> {
+                if (Desktop.isDesktopSupported()) {
+                    try {
+                        Desktop.getDesktop().browse(new URI(hl.getText()));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (URISyntaxException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            hl.setFont(Font.font("System", 15));
             readingsVboxID.getChildren().add(hl);
         }
 
         for (String link: resources.getCodes()) {
             Hyperlink hl = new Hyperlink(link);
+            // open link in default browser
+            hl.setOnAction(mouseEvent -> {
+                if (Desktop.isDesktopSupported()) {
+                    try {
+                        Desktop.getDesktop().browse(new URI(hl.getText()));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (URISyntaxException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            hl.setFont(Font.font("System", 15));
             codeVboxID.getChildren().add(hl);
         }
 
         for (String link: resources.getVideoUrls()) {
             Hyperlink hl = new Hyperlink(link);
+            // switching videos functionality
+            hl.setOnAction(mouseEvent -> {switchVideo(hl);});
+            hl.setFont(Font.font("System", 15));
             videosVboxID.getChildren().add(hl);
         }
 
-        webHolderID.getEngine().load("https://www.youtube.com/embed/M88sDrY66r8");
+        webHolderID.getEngine().load(resources.getVideoUrls().getFirst());
         webHolderID.setMaxSize(800, 480);
         webHolderID.setMinSize(480, 320);
 
         webHolderID.setPrefSize(480, 320);
+
     }
 
     @FXML
@@ -84,7 +108,6 @@ public class ResourcesController {
             zoomVboxID.setLayoutX(zoomVboxID.getLayoutX() + aspectRatio / 2);
             videosVboxID.setLayoutX(videosVboxID.getLayoutX() - aspectRatio / 2);
         }
-        System.out.println(webHolderID.getWidth());
     }
 
     @FXML
@@ -100,13 +123,18 @@ public class ResourcesController {
             zoomVboxID.setLayoutX(zoomVboxID.getLayoutX() - aspectRatio / 2);
             videosVboxID.setLayoutX(videosVboxID.getLayoutX() + aspectRatio / 2);
         }
-        System.out.println(webHolderID.getWidth());
     }
 
     @FXML
     void returnBack(ActionEvent event) throws IOException {
+        webHolderID.getEngine().load(null); // load null so it stops
         Controller controller = new Controller();
         controller.switchScene(event, "load_home_page.fxml");
+    }
+
+    @FXML
+    void switchVideo(Hyperlink hl) {
+        webHolderID.getEngine().load(hl.getText());
     }
 
 }
